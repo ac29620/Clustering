cbe3a05ee24cd7f8f71a3f232acd80eab3f5acf9
ac29620 <ac29620@gmail.com>
2018-08-02 22:13:17 -0400

    test

--- K-Means/test.py
+++ K-Means/test.py
@@ -1,1 +1,34 @@
-print ("hello")
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import rand
+
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+print(x)
+print(y)
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+plt.show()
+
+
+
+
+
+
+
+fig, ax = plt.subplots()
+for color in ['red', 'green', 'blue']:
+    n = 750
+    x, y = rand(2, n)
+    scale = 200.0 * rand(n)
+    ax.scatter(x, y, c=color, s=scale, label=color,
+               alpha=0.3, edgecolors='none')
+
+#ax.legend()
+#ax.grid(True)
+
+##plt.show()
+
